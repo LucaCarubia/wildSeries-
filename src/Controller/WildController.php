159cc2b23003358c16cc12a8c1f56bb6fb03099b
Luca Carubia <luca.carubia42@gmail.com>
2019-11-21 10:39:42 +0100

    create entity

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -8,16 +8,35 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/wild", name="wild_")
+ */
 class WildController extends AbstractController
 {
+
     /**
      * @return Response
-     * @Route("/wild", name="wild_index")
+     * @Route("/index", name="index")
      */
     public function index(): Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
+        ]);
+    }
+
+    /**
+     * @Route("/show/{slug}", requirements={"slug"="[a-z0-9-]+"}, defaults={"slug": ""}, name="show")
+     * @param string $slug
+     * @return Response
+     */
+    public function show(string $slug): Response
+    {
+        $replace = str_replace("-", " ", $slug);
+        $result = ucwords($replace);
+
+        return $this->render('wild/show.html.twig', [
+            'result' => $result,
         ]);
     }
 }
