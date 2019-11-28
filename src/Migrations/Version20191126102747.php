bc6f1e1e53ffd21dbc5701073ac92a05717bda01
Luca Carubia <luca.carubia42@gmail.com>
2019-11-28 13:41:13 +0100

    crated more entity for bidirectionnal relationship

---
+++ src/Migrations/Version20191126102747.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191126102747 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE episode ADD season_id_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE episode ADD CONSTRAINT FK_DDAA1CDA68756988 FOREIGN KEY (season_id_id) REFERENCES season (id)');
+        $this->addSql('CREATE INDEX IDX_DDAA1CDA68756988 ON episode (season_id_id)');
+        $this->addSql('ALTER TABLE season ADD program_id_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE season ADD CONSTRAINT FK_F0E45BA9E12DEDA1 FOREIGN KEY (program_id_id) REFERENCES program (id)');
+        $this->addSql('CREATE INDEX IDX_F0E45BA9E12DEDA1 ON season (program_id_id)');
+        $this->addSql('ALTER TABLE comment ADD episode_id_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C444E6803 FOREIGN KEY (episode_id_id) REFERENCES episode (id)');
+        $this->addSql('CREATE INDEX IDX_9474526C444E6803 ON comment (episode_id_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526C444E6803');
+        $this->addSql('DROP INDEX IDX_9474526C444E6803 ON comment');
+        $this->addSql('ALTER TABLE comment DROP episode_id_id');
+        $this->addSql('ALTER TABLE episode DROP FOREIGN KEY FK_DDAA1CDA68756988');
+        $this->addSql('DROP INDEX IDX_DDAA1CDA68756988 ON episode');
+        $this->addSql('ALTER TABLE episode DROP season_id_id');
+        $this->addSql('ALTER TABLE season DROP FOREIGN KEY FK_F0E45BA9E12DEDA1');
+        $this->addSql('DROP INDEX IDX_F0E45BA9E12DEDA1 ON season');
+        $this->addSql('ALTER TABLE season DROP program_id_id');
+    }
+}
