d9e70bd57d3a0243542fde8062ec7833d37f9ad9
Luca Carubia <luca.carubia42@gmail.com>
2019-12-01 14:14:30 +0100

    update

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -6,7 +6,9 @@ use App\Entity\Category;
 use App\Entity\Episode;
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Form\ProgramSearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -30,8 +32,16 @@ class WildController extends AbstractController
                 'No program found in program\'s table.'
             );
         }
+
+        $form = $this->createForm(
+            ProgramSearchType::class,
+            null,
+            ['method' => Request::METHOD_GET]
+        );
+
         return $this->render('wild/index.html.twig', [
             'programs' => $programs,
+            'form' => $form->createView(),
         ]);
     }
 
@@ -114,7 +124,7 @@ class WildController extends AbstractController
 
         $program = $repositoryProgram->findOneBy(
             ['title' => mb_strtolower($programName)
-        ]);
+            ]);
 
         $seasonProgram = $program->getSeasons();
 
@@ -131,10 +141,10 @@ class WildController extends AbstractController
      */
     public function showBySeason(int $id): Response
     {
-       $repositorySeason = $this->getDoctrine()->getRepository(Season::class);
-       $season = $repositorySeason->find($id);
-       $program = $season->getProgramId();
-       $episode = $season->getEpisodes();
+        $repositorySeason = $this->getDoctrine()->getRepository(Season::class);
+        $season = $repositorySeason->find($id);
+        $program = $season->getProgramId();
+        $episode = $season->getEpisodes();
 
         return $this->render('wild/season.html.twig', [
             'program' => $program,
